fabedc55c926861efbbd872cf179e561d772ac48
Aapo Kokko <aapo.kokko8@gmail.com>
2023-04-30 18:58:57 +0300

    First part of Caesar Cipher (encryption) and readme.

---
+++ day8_caesar_cipher/README.md
@@ -0,0 +1,11 @@
+# [Caesar Cipher](https://en.wikipedia.org/wiki/Caesar_cipher)
+
+This is an ancient cipher used by the romans during Julius Caesar's time to encrypt messages in the military. It shifts letters by a determined amount and can be decrypted by knowing the amount letters were shifted. It is not sophisticated or secure compared to modern ciphers, but is being implemented here for practice.
+
+## How to use
+
+Clone the repository and run the caesar_cipher.py with a Python interpreter.
+
+## Course solution
+
+https://replit.com/@appbrewery/caesar-cipher-4-end
---
+++ day8_caesar_cipher/caesar_cipher.py
@@ -0,0 +1,41 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+def encrypt(text, shift):
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    encrypted_text = ""
+    max_index = len(alphabet) - 1
+
+    for letter in text:
+        index = alphabet.index(letter)
+        
+        if index + shift > max_index:
+            new_index = index + shift - max_index - 1
+        else:
+            new_index = index + shift
+        encrypted_letter = alphabet[new_index]
+        encrypted_text += encrypted_letter
+    
+    print(encrypted_text)
+
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+
+encrypt(text, shift)
